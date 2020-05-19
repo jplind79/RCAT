9e1baf32ba4851f4792a36dd531741ac7d906cf8
David Lindstedt <david.lindstedt@smhi.se>
2020-05-19 13:57:57 +0200

    Extended setup.py
    Signed-off-by: David Lindstedt <david.lindstedt@smhi.se>

--- setup.py
+++ setup.py
@@ -4,19 +4,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rcat-jplind79", # Replace with your own username
+    name="RCAT",
     version="1.0.1",
     author="Petter Lind",
     author_email="petter.lind@smhi.se",
     description="Regional Climate Analysis Tool (RCAT)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jplind79/rcat",
+    url="https://github.com/jplind79/RCAT",
+    keywords=["python", "climate", "model", "interpolation", "plotting", 
+              "plots", "dask", "diagnostic", "science", "numpy"],
     packages=setuptools.find_packages(),
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
+        "Topic :: Atmospheric Science",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.0',
+    install_requires=['numpy', 'xarray', 'esmpy', 'xesmf', 'dask[complete]', 
+                      'netcdf4', 'dask-jobqueue'],
 )
